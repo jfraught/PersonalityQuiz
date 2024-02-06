9fbc0019657db51b944d36f6f2153c249591ec92
jfraught <fraughtdawg@gmail.com>
2024-02-06 11:37:49 -0700

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Jordan Fraughton on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
